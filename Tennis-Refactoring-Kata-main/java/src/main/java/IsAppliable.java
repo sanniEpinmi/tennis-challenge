ea6d3ee54a1dc6895cf6c94758081775344b3ee4
P7806 <p7806@fidelitybank.ng>
2021-09-17 07:28:27 +0100

    michael sanni tennis solution

---
+++ Tennis-Refactoring-Kata-main/java/src/main/java/IsAppliable.java
@@ -0,0 +1,9 @@
+import org.apache.commons.collections4.Predicate;
+
+public class IsAppliable implements Predicate<Score> {
+
+    public boolean evaluate(Score score) {
+        return score.isAppliable();
+    }
+
+}
---
+++ Tennis-Refactoring-Kata-main/java/src/main/java/NullScore.java
@@ -0,0 +1,16 @@
+public class NullScore extends Score{
+
+    public NullScore(Player player1, Player player2) {
+        super(player1, player2);
+    }
+
+    @Override
+    public Boolean isAppliable() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
